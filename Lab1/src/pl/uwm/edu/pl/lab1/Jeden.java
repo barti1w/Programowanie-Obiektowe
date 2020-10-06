31c3fbc5f8367cbb8b441bcf041cbef246a7915f
barti1w <57442061+barti1w@users.noreply.github.com>
2020-10-06 16:23:35 +0200

    Add files via upload

---
+++ Lab1/src/pl/uwm/edu/pl/lab1/Jeden.java
@@ -0,0 +1,54 @@
+package pl.uwm.edu.pl.lab1;
+
+public class Jeden {
+
+    public static void zad1() {
+        System.out.println("31+29+31=91dni");
+    }
+    public static void zad2() {
+        System.out.println("1+2+3+4+5+6+7+8+9+10=55");
+    }
+    public static void zad3() {
+        int a=1*2*3*4*5*6*7*8*9*10;
+        System.out.println("a");
+    }
+    public static void zad4() {
+        float a=1000;
+        float b= (float) (a+a*0.06);
+        float c= (float) (b+b*0.06);
+        float d= (float) (c+c*0.06);
+        System.out.println("Pierwszy rok " + b);
+        System.out.println("Drugi rok " + c);
+        System.out.println("Trzeci rok " + d);
+    }
+    public static void zad5() {
+        System.out.println("+------+");
+        System.out.println("| Java |");
+        System.out.println("+------+");
+    }
+    public static void zad6() {
+        System.out.println("    ///// ");
+        System.out.println("  +\"\"\"\"\"+ ");
+        System.out.println(" (| o o |)");
+        System.out.println("   | ^ |");
+        System.out.println("  | `-' |");
+        System.out.println("  +_____+ ");
+    }
+    public static void zad10() {
+        System.out.println("Czarnobyl");
+        System.out.println("Power");
+        System.out.println("Fly");
+
+    }
+    public static void zad11(){
+
+
+    System.out.println("Lata osa koło nosa\n Lata mucha koło ucha\n Lata bąk koło rąk\n Lecą ważki koło paszki\n Lata pszczoła koło czoła\n Lata mucha koło brzucha\n Lecą muszki koło nóżki\n Biegną mrówki koło główki");
+
+    }
+    public static void main (String [] args){
+        System.out.println("Dzien dobry");
+        zad11();
+}
+
+}
